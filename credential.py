6087369d61a143cbd8f24de011ac139458a1e633
Kevin Jared <kevinjaredkip@gmail.com>
2018-08-25 14:49:02 +0300

    credentials file

---
+++ credential.py
@@ -0,0 +1,29 @@
+class Credential:
+    '''
+    Class that generates new instances of credentials.
+    '''
+
+    credential_list = []  # Empty credential list
+
+    def __init__(self, first_name, last_name, number, email):
+
+        def save_credential(self):
+            '''
+            save_credential method saves credential objects into credential_list
+            '''
+
+        Credential.credential_list.append(self)
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New credential first name.
+            last_name : New credential last name.
+            number: New credential phone number.
+            email : New credential email address.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.number = number
+        self.email = email
